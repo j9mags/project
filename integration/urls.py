a1be46cb359397c397f9bed3e1bcae343ab4a635
D. H. Bahr <dhbahr@gmail.com>
2017-09-07 12:15:55 +0200

    Adding missing url pattern

--- integration/urls.py
+++ integration/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     url(r'^student/(?P<pk>.+)/$', staff.StudentReview.as_view(), name='student_review'),
     url(r'^bulk/$', staff.BulkActions.as_view(), name='students_bulk'),
     url(r'^university/$', staff.DashboardUniversity.as_view(), name='university'),
+    url(r'^courses/(?P<pk>.+)/$', staff.DashboardCourses.as_view(), name='course_review'),
     url(r'^courses/$', staff.DashboardCourses.as_view(), name='courses'),
 
     url(r'^$',
