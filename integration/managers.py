4663241c59d37fb01b2b136320c1227c68a8ad32
D. H. Bahr <dhbahr@gmail.com>
2019-02-19 16:08:10 +0100

    including missing imports

--- integration/managers.py
+++ integration/managers.py
@@ -1,4 +1,8 @@
+from django.db import connections, transaction
 from django.db.models import Q
+from django.db.models.fields import AutoField
+from django.utils.functional import partition
+
 from salesforce.backend.manager import SalesforceManager
 
 
@@ -11,21 +15,21 @@ class DefaultManager(SalesforceManager):
                 raise ValueError("Can't bulk create a multi-table inherited model")
         if not objs:
             return objs
-        
+
         self._for_write = True
         connection = connections[self.db]
         fields = self.model._meta.concrete_fields
         objs = list(objs)
 
         self._populate_pk_values(objs)
-        
+
         raise Exception()
-        
+
         with transaction.atomic(using=self.db, savepoint=False):
             objs_with_pk, objs_without_pk = partition(lambda o: o.pk is None, objs)
             if objs_with_pk:
                 self._batched_insert(objs_with_pk, fields, batch_size)
-            
+
             if objs_without_pk:
                 fields = [f for f in fields if not isinstance(f, AutoField)]
 
