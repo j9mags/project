9fbc32e105017c169a99bebe24853ad8d5734c90
D. H. Bahr <daniel.bahr@empaua.com>
2019-04-09 13:30:58 +0200

    Added html2pdf service

--- services/urls.py
+++ services/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'html2img/', views.HtmlToImageView.as_view(), name='html2image')
+    url(r'html2img/', views.HtmlToImageView.as_view(), name='html2image'),
+    url(r'html2pdf/', views.HtmlToPDFView.as_view(), name='html2pdf'),
 ]
--- services/views.py
+++ services/views.py
@@ -3,6 +3,8 @@ from django.utils.decorators import method_decorator
 from django.views.generic.base import View
 from django.views.decorators.csrf import csrf_exempt
 
+from xhtml2pdf import pisa
+from io import StringIO
 import base64
 import imgkit
 import json
@@ -27,6 +29,27 @@ class HtmlToImageView(View):
         for html in sources:
             imgkit.from_string(html, tmp_fname, options={"xvfb": ""})
             with open(tmp_fname, 'rb') as img:
-                data[img_format].append(base64.b64encode(img.read()).decode('utf-8'))
+                data[img_format].append(base64.encodebytes(img.read()))
 
         return JsonResponse(data)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class HtmlToPDFView(View):
+
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        payload = json.loads(request.body.decode('utf-8'))
+        content = payload.get('html', '')
+        
+        tmp_fname = '/tmp/out.pdf'
+        pdf = None
+        with open(tmp_fname, "w+b") as f:
+            pdf = pisa.CreatePDF(content, dest=f)
+
+        if pdf.err:
+            return JsonResponse({'error': json.dumps(pdf.err)})
+        
+        bstream = open(tmp_fname, 'rb').read()
+        return JsonResponse({'pdf': base64.encodebytes(bstream).decode('utf-8')})
